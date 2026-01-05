788051cda49e4b9348838927698f707f7524d1d6
Kapil Motwani <kapilmotwani1010@gmail.com>
2026-01-05 22:13:18 +0530

    day 1 of spring boot

---
+++ springbootpractice/src/test/java/com/kapil/springbootpractice/SpringbootpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kapil.springbootpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
